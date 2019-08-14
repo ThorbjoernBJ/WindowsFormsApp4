8c9ac6b672867d8230af6d814706a3ce26ed8968
ThorbjoernBJ <53739808+ThorbjoernBJ@users.noreply.github.com>
2019-08-14 14:11:51 +0200

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Resultat = new System.Windows.Forms.Label();
+            this.pris = new System.Windows.Forms.Label();
+            this.Banklbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RateLabel = new System.Windows.Forms.Label();
+            this.UpgradeRateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(38, 42);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(87, 42);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(43, 22);
+            this.numericUpDown3.TabIndex = 2;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(136, 42);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(43, 22);
+            this.numericUpDown4.TabIndex = 3;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(185, 42);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(43, 22);
+            this.numericUpDown5.TabIndex = 4;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(234, 42);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(43, 22);
+            this.numericUpDown6.TabIndex = 5;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(283, 42);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(43, 22);
+            this.numericUpDown7.TabIndex = 6;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Play - 5R";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Results";
+            // 
+            // Resultat
+            // 
+            this.Resultat.AutoSize = true;
+            this.Resultat.Location = new System.Drawing.Point(231, 189);
+            this.Resultat.Name = "Resultat";
+            this.Resultat.Size = new System.Drawing.Size(0, 17);
+            this.Resultat.TabIndex = 9;
+            // 
+            // pris
+            // 
+            this.pris.AutoSize = true;
+            this.pris.Location = new System.Drawing.Point(231, 294);
+            this.pris.Name = "pris";
+            this.pris.Size = new System.Drawing.Size(0, 17);
+            this.pris.TabIndex = 10;
+            // 
+            // Banklbl
+            // 
+            this.Banklbl.AutoSize = true;
+            this.Banklbl.Location = new System.Drawing.Point(512, 119);
+            this.Banklbl.Name = "Banklbl";
+            this.Banklbl.Size = new System.Drawing.Size(82, 17);
+            this.Banklbl.TabIndex = 11;
+            this.Banklbl.Text = "Bank: 100R";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(35, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 60);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // RateLabel
+            // 
+            this.RateLabel.AutoSize = true;
+            this.RateLabel.Location = new System.Drawing.Point(437, 136);
+            this.RateLabel.Name = "RateLabel";
+            this.RateLabel.Size = new System.Drawing.Size(212, 17);
+            this.RateLabel.TabIndex = 14;
+            this.RateLabel.Text = "Din gevinst rate er lige nu: x 2,5 ";
+            // 
+            // UpgradeRateButton
+            // 
+            this.UpgradeRateButton.Location = new System.Drawing.Point(38, 177);
+            this.UpgradeRateButton.Name = "UpgradeRateButton";
+            this.UpgradeRateButton.Size = new System.Drawing.Size(92, 50);
+            this.UpgradeRateButton.TabIndex = 15;
+            this.UpgradeRateButton.Text = "Upgrader Rate 50R";
+            this.UpgradeRateButton.UseVisualStyleBackColor = true;
+            this.UpgradeRateButton.Click += new System.EventHandler(this.UpgradeRateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UpgradeRateButton);
+            this.Controls.Add(this.RateLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Banklbl);
+            this.Controls.Add(this.pris);
+            this.Controls.Add(this.Resultat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Resultat;
+        private System.Windows.Forms.Label pris;
+        private System.Windows.Forms.Label Banklbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label RateLabel;
+        private System.Windows.Forms.Button UpgradeRateButton;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public int Bank = 100;
+        public Decimal Ratefaktor = 1.5m;
+        public Decimal Rate = 3m;
+        public Random rnd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            // her starter jeg med at lave de 6 forskellige udfald
+            int Udfald1 = 0;
+            int Udfald2 = 0;
+            int Udfald3 = 0;
+            int Udfald4 = 0;
+            int Udfald5 = 0;
+            int Udfald6 = 0;
+            int[] Udfald = new int[6] { 0,0,0,0,0,0};
+            
+            do
+            {
+                Udfald1 = rnd.Next(1, 15);
+                Udfald[0] = Udfald1;
+            }
+            while (Udfald1==0);
+
+            do
+            {
+                Udfald2 = rnd.Next(1, 15);
+                Udfald[1] = Udfald2;
+            }
+            while (Udfald[0] == Udfald2);
+
+            do
+            {
+                Udfald3 = rnd.Next(1, 15);
+                Udfald[2] = Udfald3;
+            }
+            while (Udfald[0] == Udfald3 || Udfald[1] == Udfald3);
+
+            do
+            {
+                Udfald4 = rnd.Next(1, 15);
+                Udfald[3] = Udfald4;
+            }
+            while (Udfald[0] == Udfald4 || Udfald[1] == Udfald4 || Udfald[2] == Udfald4);
+
+            do
+            {
+                Udfald5 = rnd.Next(1, 15);
+                Udfald[4] = Udfald5;
+            }
+            while (Udfald[0] == Udfald5 || Udfald[1] == Udfald5 || Udfald[2] == Udfald5 || Udfald[3] == Udfald5);
+
+            do
+            {
+                Udfald6 = rnd.Next(1, 15);
+                Udfald[5] = Udfald6;
+            }
+            while (Udfald[0] == Udfald6 || Udfald[1] == Udfald6 || Udfald[2] == Udfald6 || Udfald[3] == Udfald6 || Udfald[4] == Udfald6);
+
+
+
+
+
+
+
+            string Text = Udfald[0].ToString() + " " + Udfald[1].ToString() + " " + Udfald[2].ToString() + " " + Udfald[3].ToString() + " " + Udfald[4].ToString() + " " + Udfald[5].ToString();
+
+
+
+            Resultat.Text = Text;
+
+            //Her laver jeg et array over de indtastede værdier
+            decimal[] DGæt = new decimal[6] { numericUpDown1.Value, numericUpDown3.Value, numericUpDown4.Value , numericUpDown5.Value,numericUpDown6.Value,numericUpDown7.Value};
+
+            //Her vil jeg omdanne min decimal array til en int array
+            for (int i = 0; i < Udfald.Length; i++)
+            {
+                DGæt[i] = Convert.ToDecimal(DGæt[i]);
+            }
+
+            int Gevinst = 0;
+
+            // Her vil jeg sammenligne gæt og udfladet
+            for (int i = 0;i < Udfald.Length; i++)
+            {
+                
+
+                for (int j = 0; j < DGæt.Length; j++)
+                {
+                    if (Udfald[i] == DGæt[j])
+                    {
+                        Gevinst += 1;
+
+
+                        break;
+                    }
+
+                }
+
+            }
+
+            
+
+            // Gevinsten
+            pris.Text = "Gevinst" + Gevinst * Rate + 'R';
+
+            //Hvor meget du mister i din bank af at spille
+            Bank = Bank - 5;
+
+            //Selve regnestykket, men også udskrivningen.
+            Double BankNu = Convert.ToDouble(Bank) + Convert.ToDouble(Gevinst) * Convert.ToDouble(Rate);
+            Banklbl.Text = "Bank " + BankNu.ToString() + 'R';
+
+            // Her kan bankværdien hentes.
+            //Jeg vil gerne bruge Bank label som reference til en int bank værdi i stedet for at have en public int.
+            string[] BankSplit = Banklbl.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            int index1 = BankSplit[0].LastIndexOf(' ');
+            int index2 = BankSplit[0].LastIndexOf('R');
+            string BankBox = BankSplit[0].Substring(index1, index2 - index1);
+            //For at undersøge om ring virker:
+            label2.Text = "Du har lige nu " + BankBox;
+
+
+
+
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Vi converter Bank label til et string array og splitter der hvor der er et mellemrum.
+            //Dernæst tages den del af arrayet, som er efter mellemrummet.
+            string[] BankNu = Banklbl.Text.Split(' ');
+
+            label2.Text = "Du har lige nu " + BankNu[1];
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpgradeRateButton_Click(object sender, EventArgs e)
+        {
+            /*
+            //For at få fat i og bruge raten bruges følgende string, substring og decimal inddelinger
+            string[] RateSplit = RateLabel.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            int index1 = RateSplit[0].LastIndexOf('x');
+            int index2 = RateSplit[0].LastIndexOf(',');
+            int index3 = RateSplit[0].LastIndexOf(' ');
+            string NyRateOver = RateSplit[0].Substring(index1 + 2, index2 - (index1 + 2));
+            string NyRateUnder = RateSplit[0].Substring(index2 + 1, index3 - (index2 + 1));
+            Decimal NyRateO = Convert.ToDecimal(NyRateOver);
+            Decimal NyRateU = Convert.ToDecimal(NyRateUnder);
+            double Iamdead = Math.Pow(10, -1);
+            Decimal NyRate = 2 * 3;
+            */
+
+            Rate = Rate * Ratefaktor;
+
+
+            //For at undersøge om det virker:
+            RateLabel.Text = "Din gevinst rate er lige nu: x " + Rate ;
+
+
+            //Hvor meget du mister i din bank for at upgradere.
+            Bank = Bank - 50;
+
+            //Selve regnestykket, men også udskrivningen.
+            
+            Banklbl.Text = "Bank " + Bank.ToString() + 'R';
+
+                       
+            //For at undersøge om ring virker:
+            label2.Text = "Du har lige nu " + Bank + 'R';
+
+
+
+        }
+    }
+}
